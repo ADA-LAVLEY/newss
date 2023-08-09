b9274e927fc00ccbcdbd71afee4b9c9ca06c780c
ADA-LAVLEY <sitoraweb16@gmail.com>
2023-08-09 14:46:34 +0500

    yangi

---
+++ index.js
@@ -0,0 +1,33 @@
+// console.log(12);
+// console.log('hello');
+
+// let son = 10
+// son = 11
+
+// var son1 = 20
+// son1 = 12
+
+// // const son2 = 30
+// // son2 = 33
+
+// console.log(son);
+
+// var
+// const
+
+
+// data types
+
+let str = "Salom"  //String
+let num = 123  // number
+let bool = true  // false  // Boolin
+let und = undefined // undefined
+let nul = null  // null
+
+
+console.log(typeof bool);
+
+// let s = 10
+// let c = "10"
+
+// console.log(s,c);
